d2e2502e46fadc866dbfe8a74117426831d25b76
gaoyangxiaozhu <1456543560@qq.com>
2016-10-24 19:42:19 +0800

    use routename prop to get current url format

--- src/components/Pagination/index.vue
+++ src/components/Pagination/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="pagination">
     <div v-if="options.currentPage > 1" class="text-center prev active">
-      <a v-link="{ name: 'videoStorageList', params: options, query: { page: parseInt(options.currentPage) - 1 } }" class="link-title">
+      <a v-link="{ name: routename, params: options, query: { page: parseInt(options.currentPage) - 1 } }" class="link-title">
           <i class="fa fa-angle-left icon-mr"></i>
           <span>Prev Page</span>
       </a>
     </div>
     <div v-if="more" class="text-center next active">
-      <a v-link="{ name: 'videoStorageList', params: options, query: { page: parseInt(options.currentPage) + 1 } }" class="link-title">
+      <a v-link="{ name: routename, params: options, query: { page: parseInt(options.currentPage) + 1 } }" class="link-title">
           <span>Next Page</span>
           <i class="fa fa-angle-right icon-ml"></i>
       </a>
@@ -16,7 +16,7 @@
 </template>
 <script>
 export default {
-    props:['options', 'more']
+    props:['options', 'more', 'routename']
 }
 </script>
 <style lang="sass">

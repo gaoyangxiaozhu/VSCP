f0ede1ac6cd96a0bbe74b8b495d29d2b54f04891
gaoyangxiaozhu <1456543560@qq.com>
2017-03-14 13:11:15 +0800

    modify webpack.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -38,7 +38,7 @@ module.exports = {
       { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', ['css-loader?sourceMap', 'autoprefixer-loader']) },
       { test: /\.(jpe?g|png|gif)$/i, loaders: [
         'url?limit=10000&name=images/[hash:8].[name].[ext]',
-        'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
+        'image-webpack?{progressive:true, optimizationLevel: 7}'
       ]},
       { test: /\.(woff|woff2|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url?limit=10000&name=fonts/[hash:8].[name].[ext]'}
     ]
